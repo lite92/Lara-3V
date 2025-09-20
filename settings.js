ff37c89c6245eedafc15d75eade8c0e477488441
lite92 <ranasinghan598@gmail.com>
2025-09-20 19:01:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "OdhTBCKK#ooeI-F_y0nvHRXEDC8aoaEW6z9ySzMUmwr88YbRbGkU", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "bYAShQCQ#7X27jIBQAxxToedq20TNPXdicl83pwBFAZbLMVd-_eU", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "0772045782",
-DEV: process.env.DEV || "0772045782",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94766586248",
+DEV: process.env.DEV || "94766586248",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
